0fefbeaa399b9b6036a222a9264b70b5ac891845
Eduardo Orochena <mint.alchemist@gmail.com>
2021-12-22 20:58:40 -0500

    exercise 3

--- video_presentations/03_Terraform_Exercise_3/load-balancer.tf
+++ video_presentations/03_Terraform_Exercise_3/load-balancer.tf
@@ -24,7 +24,7 @@ resource "azurerm_lb_backend_address_pool" "backend_address_pool" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "interface_association" {
-  count                   = var.numVms
+  count                   = var.numVMs
   network_interface_id    = azurerm_network_interface.network_if[count.index].id
   ip_configuration_name   = "${var.resource_group}-ip-conf"
   backend_address_pool_id = azurerm_lb_backend_address_pool.backend_address_pool.id
