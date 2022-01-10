a7ac2a255325a090f6b308f9be9acaa656a31c03
Eduardo Orochena <mint.alchemist@gmail.com>
2022-01-10 11:54:32 -0500

    exercise 4 security defaults

--- video_presentations/04_Terraform_Exercise_4/storage_account.tf
+++ video_presentations/04_Terraform_Exercise_4/storage_account.tf
@@ -1,10 +1,24 @@
 resource "azurerm_storage_account" "remote_state_file" {
-  name                     = "${var.resource_group}-storage-account"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  tags                     = local.required_tags
+  name                      = "${var.resource_group}-storage-account"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  allow_blob_public_access  = false
+  is_hns_enabled            = true
+  account_replication_type  = "LRS"
+  nfsv3_enabled             = true
+  min_tls_version           = "TLS1_2"
+  enable_https_traffic_only = true
+  tags                      = local.required_tags
+}
+
+resource "azurerm_storage_account_network_rules" "storage_network_rules" {
+  storage_account_id = azurerm_storage_account.remote_state_file.id
+
+  default_action = "Deny"
+  ip_rules       = [chomp(data.http.ip_address.body)]
+  bypass         = ["Metrics", "AzureServices"]
 }
 
 resource "azurerm_storage_container" "remote_container" {
