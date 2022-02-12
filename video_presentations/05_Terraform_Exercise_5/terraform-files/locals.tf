6467fd1b786d59fc0a8f248004f2d1a75a9bfa87
Eduardo Orochena <mint.alchemist@gmail.com>
2022-02-11 20:38:20 -0500

    ex05

--- video_presentations/05_Terraform_Exercise_5/terraform-files/locals.tf
+++ video_presentations/05_Terraform_Exercise_5/terraform-files/locals.tf
@@ -1,6 +1,6 @@
 locals {
   required_tags = {
-    environment         = var.labels["environment"]
+    owner               = var.labels["owner"]
     subscription        = var.labels["subscription"]
     market              = var.labels["market"]
     resource_group_type = var.labels["resource_group_type"]
