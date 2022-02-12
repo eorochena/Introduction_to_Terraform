2c156d74a46d060dc49da93622061c8a9c95336e
Eduardo Orochena <mint.alchemist@gmail.com>
2022-02-11 19:06:37 -0500

    separate locals

--- video_presentations/05_Terraform_Exercise_5/terraform-files/main.tf
+++ video_presentations/05_Terraform_Exercise_5/terraform-files/main.tf
@@ -1,12 +1,3 @@
-locals {
-  required_tags = {
-    environment         = var.labels["environment"]
-    subscription        = var.labels["subscription"]
-    market              = var.labels["market"]
-    resource_group_type = var.labels["resource_group_type"]
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
   for_each = var.resource_information
   name     = each.key
