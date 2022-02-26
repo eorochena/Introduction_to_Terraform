2a410352924b0519a14f85251606f86de86a4562
Eduardo Orochena <mint.alchemist@gmail.com>
2022-02-26 18:22:11 -0500

    ex06

--- video_presentations/06_Terraform_Exercise_6/terraform-files/outputs.tf
+++ video_presentations/06_Terraform_Exercise_6/terraform-files/outputs.tf
@@ -3,3 +3,7 @@ output "resource_group_names" {
     for resource_group_name in azurerm_resource_group.rg : resource_group_name.tags["environment"] 
   ]
 }
+
+output "tolist_example" {
+  value = tolist([var.location])[0]
+}
